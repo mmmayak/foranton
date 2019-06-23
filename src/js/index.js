3c2c7e899f43fccbf68bc8042f9ffadbe784e605
Evhenii <1godismyname1@gmail.com>
2019-06-23 03:26:12 +0300

    end work

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@ import jQuery from "jquery";
 import popper from "popper.js";
 import bootstrap from "bootstrap";
 
-jQuery(function() {
-  jQuery("body").css("color", "blue");
+jQuery(function() {});
+
+$("#myModal").on("shown.bs.modal", function() {
+  $("#myButton").trigger("focus");
 });
